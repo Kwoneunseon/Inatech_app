b80b917cdedbda71efab0e59be1420e4a6bf1c53
eunseon <kwon9348@naver.com>
2021-09-05 11:45:58 +0900

    외부에서도 서버에 접근할 수 있도록 함
    포트포워딩 방법을 사용하려 했으나 WAMP를 사용할 떈 서버의 외부 IP를 작성해줘도 외부에서 서버의 접근이 가능했다.

--- app/src/main/java/com/example/ui/MyApplication.java
+++ app/src/main/java/com/example/ui/MyApplication.java
@@ -3,7 +3,7 @@ package com.example.ui;
 import android.app.Application;
 
 public class MyApplication extends Application {
-    public static String IP = "IP주소";
+    public static String IP = "서버 외부 IP";
 
     @Override
     public void onCreate(){
